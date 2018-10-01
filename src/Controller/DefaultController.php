919448f1576b2e24a05b34bff4b424c69e96b1a6
Jérémy Haudry <jeremy-haudry@hotmail.fr>
2018-10-01 12:13:34 +0200

    prof features

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route(path="/", methods={"GET"})
+     */
+    public function home()
+    {
+        return $this->render('default/home.html.twig');
+    }
+
+    /**
+     * @Route(path="/{name}", methods={"GET"})
+     * @param $name
+     * @return string
+     */
+    public function getName($name)
+    {
+        return $this->render('default/getName.html.twig', [
+            'name' => $name
+        ]);
+    }
+}
