617e1800bea37e86d598adbf59c572c7ad825225
Jérémy Haudry <jeremy-haudry@hotmail.fr>
2018-10-03 11:06:08 +0200

    ArticleController + Entity + Form

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/article")
+ */
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/", methods={"GET"})
+     */
+    public function index()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $articles = $em->getRepository(Article::class)->findAll();
+
+        return $this->render('Article/index.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+
+
+
+    /**
+     * @Route("/new", methods={"GET", "POST"})
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function new(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_article_index');
+        }
+
+        return $this->render('Article/new.html.twig', [
+            'Article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+}
