6ed52038335e76c04f087da57cfd8e9de45bc6ae
Younes DIOURI <younes.diouri@valeo.com>
2018-11-21 15:13:22 +0100

    register

--- src/Migrations/Version20181121104257.php
+++ src/Migrations/Version20181121104257.php
@@ -8,29 +8,31 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181114113848 extends AbstractMigration
+final class Version20181121104257 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('DROP SEQUENCE user_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE account_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE transaction_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE user_account_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('DROP SEQUENCE book_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE review_id_seq CASCADE');
         $this->addSql('CREATE TABLE account (id INT NOT NULL, name VARCHAR(150) NOT NULL, balance INT NOT NULL, state INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE account_user (account_id INT NOT NULL, user_id INT NOT NULL, PRIMARY KEY(account_id, user_id))');
         $this->addSql('CREATE INDEX IDX_10051E39B6B5FBA ON account_user (account_id)');
         $this->addSql('CREATE INDEX IDX_10051E3A76ED395 ON account_user (user_id)');
-        $this->addSql('CREATE TABLE transaction (id INT NOT NULL, account_id INT NOT NULL, amount INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE category (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE transaction (id INT NOT NULL, account_id INT NOT NULL, user_id INT NOT NULL, category_id INT NOT NULL, amount INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_723705D19B6B5FBA ON transaction (account_id)');
+        $this->addSql('CREATE INDEX IDX_723705D1A76ED395 ON transaction (user_id)');
+        $this->addSql('CREATE INDEX IDX_723705D112469DE2 ON transaction (category_id)');
         $this->addSql('CREATE TABLE user_account (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_253B48AEE7927C74 ON user_account (email)');
         $this->addSql('ALTER TABLE account_user ADD CONSTRAINT FK_10051E39B6B5FBA FOREIGN KEY (account_id) REFERENCES account (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE account_user ADD CONSTRAINT FK_10051E3A76ED395 FOREIGN KEY (user_id) REFERENCES user_account (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D19B6B5FBA FOREIGN KEY (account_id) REFERENCES account (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('DROP TABLE "user"');
+        $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D1A76ED395 FOREIGN KEY (user_id) REFERENCES user_account (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D112469DE2 FOREIGN KEY (category_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema) : void
@@ -41,15 +43,14 @@ final class Version20181114113848 extends AbstractMigration
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE account_user DROP CONSTRAINT FK_10051E39B6B5FBA');
         $this->addSql('ALTER TABLE transaction DROP CONSTRAINT FK_723705D19B6B5FBA');
+        $this->addSql('ALTER TABLE transaction DROP CONSTRAINT FK_723705D112469DE2');
         $this->addSql('ALTER TABLE account_user DROP CONSTRAINT FK_10051E3A76ED395');
-        $this->addSql('DROP SEQUENCE account_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE transaction_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE user_account_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX uniq_8d93d649e7927c74 ON "user" (email)');
+        $this->addSql('ALTER TABLE transaction DROP CONSTRAINT FK_723705D1A76ED395');
+        $this->addSql('CREATE SEQUENCE book_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE review_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('DROP TABLE account');
         $this->addSql('DROP TABLE account_user');
+        $this->addSql('DROP TABLE category');
         $this->addSql('DROP TABLE transaction');
         $this->addSql('DROP TABLE user_account');
     }
