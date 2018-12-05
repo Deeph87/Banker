216a9432a1c69ebe6a92f73822cf331f2ff399c7
Jérémy Haudry <jeremy-haudry@hotmail.fr>
2018-12-05 12:46:17 +0100

    CRUD categories done

--- src/Controller/Back/CategoryController.php
+++ src/Controller/Back/CategoryController.php
@@ -4,25 +4,30 @@ namespace App\Controller\Back;
 
 use App\Entity\Category;
 use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @Route("/category")
+ */
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/category", methods={"GET"}, name="category_index")
-     * @param Request $request
+     * @Route("/", methods="GET", name="category_index")
+     * @param CategoryRepository $categoryRepository
      * @return Response
      */
-    public function index()
+    public function index(CategoryRepository $categoryRepository)
     {
-
+        return $this->render('Back/category/index.html.twig', ['categories' => $categoryRepository->findAll()]);
     }
 
     /**
-     * @Route("/category/new", methods={"GET", "POST"}, name="category_new")
+     * @Route("/new", methods={"GET", "POST"}, name="category_new")
      * @param Request $request
      * @return Response
      */
@@ -33,39 +38,84 @@ class CategoryController extends AbstractController
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            dump("Hello");
             $em = $this->getDoctrine()->getManager();
             $em->persist($category);
             $em->flush();
 
-            return $this->redirectToRoute('category_new', [
-                'category' => $category
-            ]);
+            $this->addFlash(
+                'green',
+                'Category ' . $category->getName() . ' successfully added!'
+            );
+
+            return $this->redirectToRoute('category_index');
         }
 
         return $this->render('back/category/new.html.twig', [
-            'Article' => $category,
             'form' => $form->createView(),
         ]);
     }
 
-    public function store() {
-
+    /**
+     * @Route("/{id}", name="category_show", methods="GET")
+     * @param Category $category
+     * @return Response
+     */
+    public function show(Category $category): Response
+    {
+        return $this->render('Back/category/show.html.twig', ['category' => $category]);
     }
 
-    public function show() {
+    /**
+     * @Route("/edit/{id}", name="category_edit", methods={"GET", "POST"})
+     * @param Request $request
+     * @param Category $category
+     * @return Response
+     */
+    public function edit(Request $request, Category $category): Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash(
+                'green',
+                'Category ' . $category->getName() . ' successfully edited!'
+            );
+
+            return $this->redirectToRoute('category_index', ['id' => $category->getId()]);
+        }
+
+        return $this->render('Back/category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form->createView(),
+        ]);
     }
 
-    public function edit() {
+    /**
+     * @Route("/delete/{id}", methods="GET", name="category_delete")
+     * @param Request $request
+     * @param Category $category
+     * @return Response
+     */
+    public function delete(Request $request, Category $category): Response
+    {
+        if (!$this->isCsrfTokenValid('delete-item', $request->query->get('_token'))) {
+            throw new AccessDeniedException('Access Denied');
+        }
 
-    }
+        $categoryName = $category->getName();
 
-    public function update() {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($category);
+        $em->flush();
 
-    }
+        $this->addFlash(
+            'green',
+            'Category ' . $categoryName . ' successfully deleted!'
+        );
 
-    public function destroy() {
-
+        return $this->redirectToRoute('category_index');
     }
 }
