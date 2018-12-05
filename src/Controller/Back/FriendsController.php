087d9a944a4da332d03e3dde2b7ec6cce004f307
Younes Diouri <younesdiouri@gmail.com>
2018-12-05 10:24:37 +0100

    fixes #10 ui form

--- src/Controller/Back/FriendsController.php
+++ src/Controller/Back/FriendsController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller\Back;
 
+use App\Entity\Friendship;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use App\Entity\User;
+use Symfony\Component\Validator\Constraints\Choice;
 
 /**
  * @Route("/friends")
@@ -30,13 +33,33 @@ class FriendsController extends AbstractController
     /**
      * @Route("/add", name="friend_add", methods="GET|POST")
      */
-    public function add(Request $request): Response
+    public function add(Request $request)
     {
+        $friendship = new Friendship();
+
         $em = $this->getDoctrine()->getManager();
         $users = $em->getRepository(User::class)->findAllExceptMe($this->getUser()->getId());
+        $form = $this->createFormBuilder($users)
+            ->add('pseudo', null, array('label' => 'Entrer son pseudo'))
+            ->add('Ajouter', SubmitType::class, array('label' => 'Add Friend'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $friendship = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            // $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($task);
+            // $entityManager->flush();
+
+            return $this->redirectToRoute('task_success');
+        }
 
         return $this->render('friends/add.html.twig', [
-            'users' => $users,
+            'form' => $form->createView(),
         ]);
     }
 }
