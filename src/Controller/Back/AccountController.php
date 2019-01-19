581ebf0e648d851207b860d6d63d149ac93ebc38
Younes Diouri <younesdiouri@gmail.com>
2019-01-19 16:44:12 +0100

    fix #25

--- src/Controller/Back/AccountController.php
+++ src/Controller/Back/AccountController.php
@@ -3,10 +3,15 @@
 namespace App\Controller\Back;
 
 use App\Entity\Account;
+use App\Entity\Friendship;
 use App\Entity\User;
 use App\Form\AccountType;
 use App\Repository\AccountRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,6 +24,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
  */
 class AccountController extends AbstractController
 {
+    CONST PENDING = 0;
+    CONST ACCEPTED = 1;
+    CONST REFUSED = 2;
+
     /**
      * @Route("/", name="account_index", methods="GET")
      */
@@ -33,12 +42,40 @@ class AccountController extends AbstractController
     public function new(Request $request): Response
     {
         $account = new Account();
-        $form = $this->createForm(AccountType::class, $account);
+        $myFriends = [];
+        $em = $this->getDoctrine()->getManager();
+        $meFriendships = $em->getRepository(Friendship::class)->findBy(['me' => $this->getUser(), 'status' => self::ACCEPTED]);
+        $otherFriendships = $em->getRepository(Friendship::class)->findBy(['friend' => $this->getUser(), 'status' => self::ACCEPTED]);
+
+        foreach ($meFriendships as $f)
+            $myFriends[$f->getFriend()->getPseudo()] = $f->getFriend()->getPseudo();
+
+        foreach ($otherFriendships as $f)
+            $myFriends[$f->getMe()->getPseudo()] = $f->getMe()->getPseudo();
+
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class)
+            ->add('balance', NumberType::class)
+            ->add('friends', ChoiceType::class, array(
+                'label' => 'Entrer son pseudo',
+                'multiple' => true,
+                'choices' => $myFriends
+            ))
+            ->add('Créer', SubmitType::class, array('label' => 'Create an Account'))
+            ->getForm();
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $account->addUser($this->getUser());
+
+            foreach ($form->getData()["friends"] as $f){
+                $user = $em->getRepository(User::class)->findOneBy(['pseudo' => $f]);
+                $account->addUser($user);
+            }
+
+            $account->setName($form->getData()["name"]);
+            $account->setBalance($form->getData()["balance"]);
             $em->persist($account);
             $em->flush();
 
@@ -49,6 +86,7 @@ class AccountController extends AbstractController
             'account' => $account,
             'form' => $form->createView(),
         ]);
+
     }
 
     /**
