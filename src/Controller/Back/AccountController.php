0adca360f73c5a39fdd056682e9a3f62f99f30ac
RobinTHOREL <thorelrobin@yahoo.fr>
2019-01-24 16:03:36 +0100

    add account page

--- src/Controller/Back/AccountController.php
+++ src/Controller/Back/AccountController.php
@@ -102,7 +102,7 @@ class AccountController extends AbstractController
 
         return $this->render('account/show.html.twig', [
             'account' => $account,
-            'transactions' => $transactions
+            'transactions' => array_reverse($transactions)
         ]);
     }
 
